02a4af37f72fdcbf846a8ed87a52d0a8ad9402d9
Danya Ashihin <danyadefault@gmail.com>
2025-08-17 14:41:31 +0300

    feature/5 fixed comment

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM openjdk:21-slim
 WORKDIR /app
 
 
-COPY target/interview-service-0.0.1.jar app.jar
+COPY target/*.jar app.jar
 
 
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/ru/dev/runtime/panic/interview/domain/BaseEntity.java
+++ src/main/java/ru/dev/runtime/panic/interview/domain/BaseEntity.java
@@ -1,11 +1,14 @@
-package domain;
+package ru.dev.runtime.panic.interview.domain;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.ZonedDateTime;
+import java.time.OffsetDateTime;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -14,43 +17,38 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(nullable = false, updatable = false)
-    private ZonedDateTime created;
+    @CreatedDate
+    @Column
+    private OffsetDateTime created;
 
-    @Column(nullable = true)
-    private ZonedDateTime updated;
+    @LastModifiedDate
+    @Column
+    private OffsetDateTime updated;
 
-    @Column(nullable = true)
-    private ZonedDateTime deleted;
+    @Column
+    private OffsetDateTime deleted;
 
     @Version
     private Long version;
 
-    @PrePersist
-    public void prePersist() {
-        this.created = ZonedDateTime.now();
-    }
-    @PreUpdate
-    public void preUpdate() {
-        this.updated = ZonedDateTime.now();
-    }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         BaseEntity that = (BaseEntity) o;
-        return Objects.equals(id, that.id) && Objects.equals(created, that.created) && Objects.equals(updated, that.updated) && Objects.equals(deleted, that.deleted);
+        return Objects.equals(id, that.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, created, updated, deleted);
+        return Objects.hashCode(id);
     }
 
     @Override
